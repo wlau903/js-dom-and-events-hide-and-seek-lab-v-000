3bd0e026559943eae33dd20392f73e5385df8202
Wendy Lau <wlau903@gmail.com>
2017-12-14 06:42:41 +0000

    Done.

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ function increaseRankBy(n) {
   const lis = document.querySelectorAll('.ranked-list');
 
   for (let i = 0; i < lis.length; i++) {
-    lis[i].innerHTML = parseInt(lis[i].innerHTML + n).toString();
+    lis[i].innerHTML = parseInt(lis[i].innerHTML + n);
   }
 }
